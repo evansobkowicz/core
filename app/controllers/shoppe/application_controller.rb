725cf9612a9e48682ede493aed4c6dbebb9af18c
Evan Sobkowicz <evansobkowicz@gmail.com>
2014-11-05 12:19:46 -0500

    removing auth

--- app/controllers/shoppe/application_controller.rb
+++ app/controllers/shoppe/application_controller.rb
@@ -1,12 +1,12 @@
 module Shoppe
   class ApplicationController < ActionController::Base
-    
-    before_filter :login_required
-    
+
+    before_action :authenticate_user!
+
     rescue_from ActiveRecord::DeleteRestrictionError do |e|
       redirect_to request.referer || root_path, :alert => e.message
     end
-    
+
     rescue_from Shoppe::Error do |e|
       @exception = e
       render :layout => 'shoppe/sub', :template => 'shoppe/shared/error'
@@ -16,31 +16,31 @@ module Shoppe
 
     def login_required
       unless logged_in?
-        redirect_to login_path
+        redirect_to '/'
       end
     end
 
     def logged_in?
-      current_user.is_a?(User)
-    end
-    
-    def current_user
-      @current_user ||= login_from_session || login_with_demo_mdoe || :false
+      user_signed_in?
     end
 
-    def login_from_session
-      if session[:shoppe_user_id]
-        @user = User.find_by_id(session[:shoppe_user_id])
-      end
-    end
-    
-    def login_with_demo_mdoe
-      if Shoppe.settings.demo_mode?
-        @user = User.first
-      end
-    end
-    
-    helper_method :current_user, :logged_in?
-    
+    # def current_user
+    #   @current_user ||= login_from_session || login_with_demo_mdoe || :false
+    # end
+
+    # def login_from_session
+    #   if session[:shoppe_user_id]
+    #     @user = User.find_by_id(session[:shoppe_user_id])
+    #   end
+    # end
+
+    # def login_with_demo_mdoe
+    #   if Shoppe.settings.demo_mode?
+    #     @user = User.first
+    #   end
+    # end
+
+    # helper_method :current_user, :logged_in?
+
   end
 end
